651aa62f29f5c1a502e754de6587c27fe6c7d08c
Harshad Loya <hloya1@binghamton.edu>
2017-03-27 06:13:00 -0400

    Tabs with content added for Disclaimer, References and About App

--- app/src/main/java/edu/binghamton/hloya1/obwheels/MainScreen.java
+++ app/src/main/java/edu/binghamton/hloya1/obwheels/MainScreen.java
@@ -3,7 +3,9 @@ package edu.binghamton.hloya1.obwheels;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -20,14 +22,17 @@ public class MainScreen extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_disclaimer:
+                    mTextMessage.setText(R.string.disclaimer_text);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_aboutApp:
+                    mTextMessage.setText(R.string.aboutapp_text);
                     return true;
-                case R.id.navigation_AboutUs:
-                    mTextMessage.setText(R.string.title_aboutus);
+                case R.id.navigation_references:
+                    mTextMessage.setText(R.string.ref_text);
+                    return true;
+                case R.id.navigation_moreApps:
+                    mTextMessage.setText(R.string.title_moreApps);
                     return true;
             }
             return false;
@@ -41,6 +46,7 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.main_screen);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        //mTextMessage.setMovementMethod(new ScrollingMovementMethod());
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
