b107a1d60beac9593bdaa5df94e45f14a9de806e
HarshadLoya <harshadloya@gmail.com>
2017-04-01 07:45:36 -0400

    MoreApps Screen Content added

--- app/src/main/java/edu/binghamton/hloya1/obwheels/MoreAppsScreen.java
+++ app/src/main/java/edu/binghamton/hloya1/obwheels/MoreAppsScreen.java
@@ -1,5 +1,7 @@
 package edu.binghamton.hloya1.obwheels;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +9,7 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -15,21 +18,24 @@ import android.widget.TextView;
 
 public class MoreAppsScreen extends Fragment
 {
-    private TextView mTextMessage;
-
+    private Button button;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-
         View view = inflater.inflate(R.layout.moreapps_screen, container, false);
-
-        mTextMessage = (TextView) view.findViewById(R.id.message);
-        mTextMessage.setText(R.string.title_moreApps);
-        mTextMessage.setMovementMethod(new ScrollingMovementMethod());
+        button = (Button) view.findViewById(R.id.moreAppsButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Uri uri = Uri.parse("https://resourcelibrary.stfm.org/HigherLogic/System/DownloadDocumentFile.ashx?DocumentFileKey=72a92740-16aa-6499-f3b1-ae4dc8c2e119&forceDialog=1");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
 
         return view;
-
     }
 
 }
